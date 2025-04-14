7f9cc0bcaa6c8205f5385ce8d5e08d0deeea2a51
Girish Gondaliya <rw3.girish.gk@gmail.com>
2025-04-14 16:32:29 +0530

    local user

--- admin-panel/server.js
+++ admin-panel/server.js
@@ -27,6 +27,7 @@ app.use(session({
 
 app.use(passport.session());
 app.use(passport.initialize());
+app.use(passport.setAuthenticateUser);
 
 // routes
 app.use("/", require('./routes/index.routes'));
